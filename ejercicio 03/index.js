e4db84d2ea8a30f1d9e4762d4fac813a49afda4f
jessy006 <jessy.ortiz1210@gmail.com>
2024-11-28 10:11:17 -0500

    first commit

---
+++ ejercicio 03/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let a = 5;
+let b = 10;
+console.log("Antes del intercambio: a =", a, ", b =", b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("Después del intercambio: a =", a, ", b =", b);
